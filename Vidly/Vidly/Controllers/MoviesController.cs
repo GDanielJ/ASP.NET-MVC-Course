047dfcfd34bb94f018206458d6d050b801be52c9
Daniel Johansson <johansson.gd@gmail.com>
2019-03-12 18:08:18 +0100

    Lagt till Edit och Index actionresults i MoviesController.

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -13,7 +13,22 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
+
             return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("Id = " + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("pageIndex={0} sortBy={1}", pageIndex, sortBy));
         }
     }
 }
