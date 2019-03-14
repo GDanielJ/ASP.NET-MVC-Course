c6bcdef3f1a5709bbefdc9a0aa14703cb4ebfcf0
Daniel Johansson <johansson.gd@gmail.com>
2019-03-14 21:15:01 +0100

    Börjat med excresie 2.19.

---
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers/Index
+        public ViewResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        private IEnumerable<Customer> GetCustomers() // TODO - forts med tillhörande View.
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Smith" },
+                new Customer { Id = 2, Name = "Mary Williams" }
+            };
+        }
+    }
+}
