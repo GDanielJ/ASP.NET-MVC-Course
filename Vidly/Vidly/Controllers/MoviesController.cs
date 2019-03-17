d93d80f101b094fcb27392bf6de5c60ace747e7c
Daniel Johansson <johansson.gd@gmail.com>
2019-03-17 21:33:05 +0100

    Klar med övningen. 2.19.

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -18,15 +18,6 @@ namespace Vidly.Controllers
             return View(movies);
         }
 
-        private IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>
-            {
-                new Movie { Id = 1, Name="Shrek" },
-                new Movie { Id = 2, Name = "Wall-e" }
-            };
-        }
-
         // GET: Movies/Random
         public ActionResult Random()
         {
@@ -51,19 +42,28 @@ namespace Vidly.Controllers
             return Content("Id = " + id);
         }
 
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-            return Content(String.Format("pageIndex={0} sortBy={1}", pageIndex, sortBy));
-        }
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+        //    if (String.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
+        //    return Content(String.Format("pageIndex={0} sortBy={1}", pageIndex, sortBy));
+        //}
 
         [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content($"{year}/{month}");
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name="Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
         }
     }
 }
