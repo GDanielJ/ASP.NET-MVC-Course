38ab0210e93d396c347cff886acb9b49c4b47639
Daniel Johansson <johansson.gd@gmail.com>
2019-04-15 19:55:12 +0200

    Klar med section 3 innan exercises.

--- Vidly2/Vidly2/Controllers/CustomersController.cs
+++ Vidly2/Vidly2/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,10 +10,22 @@ namespace Vidly2.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customer/Index
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
@@ -20,7 +33,7 @@ namespace Vidly2.Controllers
         // GET: Customer/Details
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
             {
@@ -28,15 +41,6 @@ namespace Vidly2.Controllers
             }
 
             return View(customer);
-        }
-
-        public IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer {Id = 1, Name = "John Smith"},
-                new Customer {Id = 2, Name = "Mary Williams"}
-            };
         }
     }
 }
