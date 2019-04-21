05d474034268c1a10e1c20317b74f0689ee433c1
Daniel Johansson <johansson.gd@gmail.com>
2019-04-21 14:40:07 +0200

    Klar med exercise 3.3.

--- Vidly2/Vidly2/Controllers/MoviesController.cs
+++ Vidly2/Vidly2/Controllers/MoviesController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Entity;
 using System.Web;
 using System.Web.Mvc;
 using Vidly2.Models;
@@ -10,22 +11,39 @@ namespace Vidly2.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Movies/Index
         public ViewResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
         }
 
-        private IEnumerable<Movie> GetMovies()
+        // GET: Movies/Details
+        public ActionResult Details(int id)
         {
-            return new List<Movie>
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
             {
-                new Movie {Id = 1, Name = "Shrek"},
-                new Movie {Id = 2, Name = "Wall-E" }
-            };
+                return HttpNotFound();
+            }
+                
+            return View(movie);
         }
+
 
         // GET: Movies/Random
         public ActionResult Random()
